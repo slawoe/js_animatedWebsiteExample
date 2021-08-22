c50ed6e24a1fdac1c87bd2bc190e6330f768675c
slawoe <info@slawomirernst.de>
2021-08-22 12:45:55 +0200

    Add slideScene

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
   controller = new ScrollMagic.Controller();
   const sliders = document.querySelectorAll(".slide");
   const nav = document.querySelector(".nav-header");
-  sliders.forEach((slide) => {
+  sliders.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
@@ -16,6 +17,18 @@ function animateSlides() {
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
     slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.75");
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.55,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(controller);
   });
 }
 
